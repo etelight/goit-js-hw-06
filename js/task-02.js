adedfaa50adefc43455b8da751e4a1ffd4ad3b4c
etelight <etelight@gmail.com>
2023-06-01 15:32:38 +0300

    add 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulEles = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const liEle = document.createElement("li");
+  liEle.textContent = ingredient;
+  liEle.classList.add("item");
+  ulEles.appendChild(liEle);
+});
