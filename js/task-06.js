1cdf8cf9b0ea6db257de338e82b305ebb21fa397
etelight <etelight@gmail.com>
2023-06-07 17:22:46 +0300

    update task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,6 +1,7 @@
 const validationInput = document.querySelector("#validation-input");
 validationInput.addEventListener("blur", (event) => {
-  if (event.target.value.length === Number(validationInput.dataset.length)) {
+  const trimValue = event.target.trim();
+  if (trimValue.value.length === Number(validationInput.dataset.length)) {
     event.target.classList.add("valid");
     event.target.classList.remove("invalid");
   } else {
