b58b2ad7656d8fcefbd1f45867f278331d6e94cc
etelight <etelight@gmail.com>
2023-06-07 18:44:13 +0300

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginFormEl = document.querySelector(".login-form");
+const emailInputEl = loginFormEl.elements.email;
+const passwordInputEl = loginFormEl.elements.password;
 
+loginFormEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (emailInputEl.value && passwordInputEl.value) {
+    console.log({
+      email: emailInputEl.value,
+      password: passwordInputEl.value,
+    });
+    loginFormEl.reset();
+    return;
+  }
+
+  alert("Заповніть усі поля!");
+});
